9ecee7fdb57aef5a592b033d35da95d6cc6110a2
Majkie <majkie.tros@gmail.com>
2022-05-27 20:18:55 +0200

    WIP

--- pages/login.vue
+++ pages/login.vue
@@ -25,10 +25,10 @@
                 </form>
                 <hr class="dark:border-slate-700" />
                 <div class="flex flex-col space-y-4">
-                    <a href="#" class="text-slate-900 bg-white hover:bg-slate-100 border border-slate-200 focus:ring-4 focus:outline-none focus:ring-slate-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-slate-600 dark:bg-slate-800 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700">
+                    <a href="http://localhost:8000/auth/twitch" class="text-slate-900 bg-white hover:bg-slate-100 border border-slate-200 focus:ring-4 focus:outline-none focus:ring-slate-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-slate-600 dark:bg-slate-800 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700">
                         Login using Twitch
                     </a>
-                    <a href="#" class="text-slate-900 bg-white hover:bg-slate-100 border border-slate-200 focus:ring-4 focus:outline-none focus:ring-slate-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-slate-600 dark:bg-slate-800 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700">
+                    <a href="http://localhost:8000/auth/patreon" class="text-slate-900 bg-white hover:bg-slate-100 border border-slate-200 focus:ring-4 focus:outline-none focus:ring-slate-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-slate-600 dark:bg-slate-800 dark:border-slate-700 dark:text-white dark:hover:bg-slate-700">
                         Login using Patreon
                     </a>
                 </div>
