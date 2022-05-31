2b8e30eaab6f93c64170b42961c37afe5554c161
Majkie <majkie.tros@gmail.com>
2022-05-31 21:28:49 +0200

    WS For tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        
+
     </div>
 </template>
 
@@ -8,4 +8,40 @@
 useHead({
     title: 'Welcome'
 })
+
+
+onMounted(() => {
+    const socket = new WebSocket('ws://localhost:8000');
+
+
+    socket.onopen = () => {
+
+        console.log('connected');
+
+        socket.send(
+            JSON.stringify({
+                event: 'join',
+                data: {
+                    channel: 'majciiik',
+                    user: 'majciiik',
+                    pro: false,
+                },
+            })
+        );
+
+        setTimeout(() => {
+            socket.send(
+                JSON.stringify({
+                    event: 'leave',
+                    data: 'testing',
+                })
+            );
+        }, 5000);
+
+        socket.onmessage = (event) => {
+            console.log(JSON.parse(event.data).data);
+        }
+    };
+})
+
 </script>
